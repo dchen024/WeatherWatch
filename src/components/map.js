4731b8f3d9d23272fe8da4ceca763b0883c66183
Daniel Chen <dchen024@citymail.cuny.edu>
2023-06-18 03:42:23 -0400

    added nav bar to map

--- src/components/map.js
+++ src/components/map.js
@@ -44,6 +44,13 @@ function Map({ selectedCity, children }) {
   }, [selectedCity]);
 
   return (
+  <>
+    <nav className="news-navbar">
+        <a href="/" className="news-navbar-brand">Home</a>
+        <a href="/weather" className="news-navbar-brand">Weather</a>
+        <a href="/news" className="news-navbar-brand">News</a>
+        <h2 className='news-title'>Map</h2>
+      </nav>
     <div style={{ height: "100%", width: "100%" }}>
       <MapGL
         {...viewport}
@@ -93,6 +100,7 @@ function Map({ selectedCity, children }) {
         ></div>
       )}
     </div>
+  </>
   );
 };
 
